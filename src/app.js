bb30a08db9821ac5e86b875a0e3861ff20da51a0
andreleox <andreleox@gmail.com>
2020-08-21 09:21:42 -0300

    Implement and test app

--- src/app.js
+++ src/app.js
@@ -12,80 +12,67 @@ app.use(cors());
 
 const repositories = [];
 
-// Create middleware function to check valid repository id
-function validateRepositoryId (request, response, next){
-  //TODO
-  // Get id from request 
-  // Check if it is uuid isuid(id), else return status(400)
-  // Return next 
-}
-
-
 app.get("/repositories", (request, response) => {
+
   return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // Get data from body  
+
   const { title, url, techs } = request.body;
-  const likes = 0;
   
-  // Set one specific repository, with a new uuid and 0 likes 
+  // The only way to change likes property is using appropriate resource 
+  const likes = 0;
+
   const repository = { id: uuid(), title, url, techs, likes }
   
-  // Push one element to array 
   repositories.push(repository);
 
-  // Return a json with the brand new repository 
   return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // Get data from request
+
   const { id } = request.params;
   const { title, url, techs } = request.body;
   
-  // Check if repository exists, else return error
   const repositoryIndex = repositories.findIndex( (repo) => repo.id === id ); 
   
   if (repositoryIndex < 0) {
     return response.status(400).json({ error: `id not found: ${id}`})
   }
  
-  // Get current likes and avoid changes
+  // Saving likes before change object
   const { likes } = repositories[repositoryIndex]; 
 
-  // Update repository 
   repository = { id, title, url, techs, likes }
 
   repositories[repositoryIndex] = repository;
 
-  // Rerturn a json with modified repository 
   return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // Get id from request and repository index 
   const { id } = request.params; 
   const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
 
-  // Check if it exists, else return error 
   if (repositoryIndex < 0 ) {
     return response.status(400).json({ error: "Invalid id"})
   }
 
-  // Delete repository and return 204
+  // Delete repository and return 204: Ok, but no body
   repositories.splice(repositoryIndex, 1); 
   
   return response.status(204).send(); 
 });
 
+// This is the only way to add one to likes property  
 app.post("/repositories/:id/like", (request, response) => {
-  // Get id from request and repository index 
   const { id } = request.params; 
   const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
 
-  // Check if it exists, else return error 
   if (repositoryIndex < 0 ) {
     return response.status(400).json({ error: "Invalid id"})
   }
@@ -96,8 +83,8 @@ app.post("/repositories/:id/like", (request, response) => {
   // Add +1 to likes 
   likes += 1; 
 
-  // Update repository and return json 
   repositories[repositoryIndex] = { id, title, url, techs, likes }
+
   return response.json(repositories[repositoryIndex]);
     
 });
