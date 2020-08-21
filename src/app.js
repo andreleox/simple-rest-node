8e5890375c9a7b672e1e754e88d8d94087dd2fa9
andreleox <andreleox@gmail.com>
2020-08-21 09:55:00 -0300

    App code enhancement

--- src/app.js
+++ src/app.js
@@ -21,11 +21,8 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
 
   const { title, url, techs } = request.body;
-  
-  // The only way to change likes property is using appropriate resource 
-  const likes = 0;
 
-  const repository = { id: uuid(), title, url, techs, likes }
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
   
   repositories.push(repository);
 
@@ -41,13 +38,16 @@ app.put("/repositories/:id", (request, response) => {
   const repositoryIndex = repositories.findIndex( (repo) => repo.id === id ); 
   
   if (repositoryIndex < 0) {
-    return response.status(400).json({ error: `id not found: ${id}`})
+    return response.status(400).json({ error: `Repository does not exist: ${id}`})
   }
  
-  // Saving likes before change object
-  const { likes } = repositories[repositoryIndex]; 
-
-  repository = { id, title, url, techs, likes }
+  repository = { 
+    id, 
+    title, 
+    url, 
+    techs, 
+    likes: repositories[repositoryIndex].likes
+  }
 
   repositories[repositoryIndex] = repository;
 
@@ -59,10 +59,10 @@ app.delete("/repositories/:id", (request, response) => {
   const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
 
   if (repositoryIndex < 0 ) {
-    return response.status(400).json({ error: "Invalid id"})
+    return response.status(400).json({ error: "Repository does not exists"})
   }
 
-  // Delete repository and return 204: Ok, but no body
+  // Delete repository and return 204: no content
   repositories.splice(repositoryIndex, 1); 
   
   return response.status(204).send(); 
@@ -80,10 +80,7 @@ app.post("/repositories/:id/like", (request, response) => {
   const { title, url, techs } = repositories[repositoryIndex];
   var { likes } = repositories[repositoryIndex];
 
-  // Add +1 to likes 
-  likes += 1; 
-
-  repositories[repositoryIndex] = { id, title, url, techs, likes }
+  repositories[repositoryIndex].likes++;
 
   return response.json(repositories[repositoryIndex]);
     
